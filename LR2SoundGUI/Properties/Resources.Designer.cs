661dd8a1ffcf629d23f17c0acc1476f66baed820
xtremealu <samardzija.robert@gmail.com>
2020-07-02 19:41:43 +0200

    Complete UI rewrite

--- LR2SoundGUI/Properties/Resources.Designer.cs
+++ LR2SoundGUI/Properties/Resources.Designer.cs
@@ -69,5 +69,15 @@ namespace LR2SoundGUI.Properties {
                 return ((System.Drawing.Icon)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap lego_logo_512 {
+            get {
+                object obj = ResourceManager.GetObject("lego_logo_512", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
